31c5f9108bb470b064aed4eeec30964b64d7b7c4
Sanket Rajendra Shah <sanketshah812@gmail.com>
2023-07-30 23:11:34 +0200

    Added timer

---
+++ build/CMakeFiles/voxelize_cuda.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sanket/Desktop/Projects/voxelize-cuda/main.cpp" "/home/sanket/Desktop/Projects/voxelize-cuda/build/CMakeFiles/voxelize_cuda.dir/main.cpp.o"
+  "/home/sanket/Desktop/Projects/voxelize-cuda/src/preprocess.cpp" "/home/sanket/Desktop/Projects/voxelize-cuda/build/CMakeFiles/voxelize_cuda.dir/src/preprocess.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-11.1/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/voxelize_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxelize_cuda.dir/src/voxelize_cuda_generated_preprocess_kernel.cu.o"
+  "CMakeFiles/voxelize_cuda.dir/main.cpp.o"
+  "CMakeFiles/voxelize_cuda.dir/src/preprocess.cpp.o"
+  "voxelize_cuda.pdb"
+  "voxelize_cuda"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/voxelize_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
